b35046e175eb50f94e2994b28064d7f7918ca68f
dmi3 <dima@dmi3.net>
2021-10-12 19:35:04 +0300

    Show error notification

--- error-count/background.js
+++ error-count/background.js
@@ -1,6 +1,15 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
+    chrome.notifications.create('notification.warning', {
+      iconUrl: chrome.runtime.getURL('logo.png'),
+      title: 'Got new JavaScript error',
+      type: 'basic',
+      message: '',
+      buttons: [],
+      priority: 2,
+    }, function() {});
     chrome.browserAction.setBadgeBackgroundColor({tabId: sender.tab.id, color: '#FF0000' });
     chrome.browserAction.setBadgeText({tabId: sender.tab.id, text: '!' });
+    
   }
 );
